c264e774a35846601f6afbdaaca64d6c82bd5a8e
Harshal Gunjal <23f2001627@ds.study.iitm.ac.in>
2024-02-05 06:24:15 +0530

    initial commit

--- 16_asynchronous/Promises/promise.js
+++ 16_asynchronous/Promises/promise.js
@@ -1,35 +1,67 @@
-console.log('This is Promises');
+const promiseOne = new Promise(function (resolve, reject) {
+    //Do an async task
+    // DB calls, cryptography, network
+    setTimeout(function () {
+        console.log("Async task is compelete");
+        resolve();
+    }, 1000);
+});
 
-let prom1 = new Promise((resolve, reject) => {
-    let a = Math.random();
-    if (a < 0.5) {
-        reject("No random number was not supporting you")
-    }
-    else {
-        setTimeout(() => {
-            console.log("Yes I am done")
-            resolve("Harry")
-        }, 3000);
-    }
-})
+promiseOne.then(function () {
+    console.log("Promise consumed");
+});
 
-let prom2 = new Promise((resolve, reject) => {
-    let a = Math.random();
-    if (a < 0.5) {
-        reject("No random number was not supporting you 2")
-    }
-    else {
-        setTimeout(() => {
-            console.log("Yes I am done 2")
-            resolve("Harry 2")
-        }, 1000);
-    }
-})
 
- 
-let p3 = Promise.race([prom1, prom2])
-p3.then((a)=>{
-    console.log(a)
-}).catch(err=>{
-    console.log(err)
-})
+
+
+
+new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        console.log("Async task 2");
+        resolve();
+    }, 1000);
+}).then(function () {
+    console.log("Async 2 resolved");
+});
+
+
+
+
+
+const promiseThree = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve({ username: "Chai", email: "chai@example.com" });
+    }, 1000);
+});
+
+promiseThree.then(function (user) {
+    console.log(user);
+});
+
+
+
+
+
+const promiseFour = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        let error = true;
+        if (!error) {
+            resolve({ username: "hitesh", password: "123" });
+        } else {
+            reject("ERROR: Something went wrong");
+        }
+    }, 1000);
+});
+
+promiseFour
+    .then((user) => {
+        console.log(user);
+        return user.username;
+    })
+    .then((username) => {
+        console.log(username);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    .finally(() => console.log("The promise is either resolved or rejected"));
